3a110d8e534342d267ca19fda394f9191ccc4a1b
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-11-26 00:13:55 +0530

    new root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,16 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# added manually for reading static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    # '/var/www/static/',
-]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATICFILES_DIRS=[STATIC_DIR,]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
